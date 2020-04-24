53d1d3cc5a9160b176c62a7ec4561b2f649e3f5a
ThaNinjaPower <kevinvannguyen1997@gmail.com>
2020-04-24 11:10:32 -0500

    Show webpage

--- server.js
+++ server.js
@@ -4,6 +4,7 @@ var app = express();
 var PORT = process.env.PORT || 8080;
 app.use(express.urlencoded({extended: true}));
 app.use(express.json());
+app.use(express.static("public"));
 
 app.get("/", function(req, res) {
     res.sendFile(path.join(__dirname, "/public/index.html"));
